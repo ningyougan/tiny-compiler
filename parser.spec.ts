c3361e361e636a964b79d2aa9ba9b281c61a6e6f
Polaris <54269504@qq.com>
2022-09-25 14:00:46 +0800

    refa

--- parser.spec.ts
+++ parser.spec.ts
@@ -1,5 +1,6 @@
-import { describe, expect, it, test } from "vitest";
-import { parser, NodeTypes } from "./parser";
+import { describe, expect, it } from "vitest";
+import { NodeTypes } from "./ast";
+import { parser } from "./parser";
 import { TokenTypes } from "./tokenizer";
 
 describe("parser", () => {
@@ -16,14 +17,14 @@ describe("parser", () => {
       { type: TokenTypes.Paren, value: ")" },
     ];
     const ast = {
-      type: NodeTypes.Root,
+      type: NodeTypes.Program,
       body: [
         {
           type: NodeTypes.CallExpression,
           name: "add",
           params: [
             {
-              type: NodeTypes.Number,
+              type: NodeTypes.NumberLiteral,
               value: "2",
             },
             {
@@ -31,11 +32,11 @@ describe("parser", () => {
               name: "subtract",
               params: [
                 {
-                  type: NodeTypes.Number,
+                  type: NodeTypes.NumberLiteral,
                   value: "4",
                 },
                 {
-                  type: NodeTypes.Number,
+                  type: NodeTypes.NumberLiteral,
                   value: "2",
                 },
               ],
@@ -46,50 +47,65 @@ describe("parser", () => {
     };
     expect(parser(tokens)).toEqual(ast);
   });
-});
-test("number", () => {
-  const tokens = [
-    {
-      type: TokenTypes.Number,
-      value: "2",
-    },
-  ];
-  const ast = {
-    type: NodeTypes.Root,
-    body: [{ type: NodeTypes.Number, value: "2" }],
-  };
 
-  expect(parser(tokens)).toEqual(ast);
-});
+  it("number", () => {
+    const tokens = [{ type: TokenTypes.Number, value: "2" }];
 
-test("callExpression", () => {
-  const tokens = [
-    { type: TokenTypes.Paren, value: "(" },
-    { type: TokenTypes.Name, value: "add" },
-    { type: TokenTypes.Number, value: "2" },
-    { type: TokenTypes.Number, value: "4" },
-    { type: TokenTypes.Paren, value: ")" },
-  ];
+    const ast = {
+      type: NodeTypes.Program,
+      body: [
+        {
+          type: NodeTypes.NumberLiteral,
+          value: "2",
+        },
+      ],
+    };
+    expect(parser(tokens)).toEqual(ast);
+  });
 
-  const ast = {
-    type: NodeTypes.Root,
-    body: [
-      {
-        type: NodeTypes.CallExpression,
-        name: "add",
-        params: [
-          {
-            type: NodeTypes.Number,
-            value: "2",
-          },
-          {
-            type: NodeTypes.Number,
-            value: "4",
-          },
-        ],
-      },
-    ],
-  };
+  it("name", () => {
+    const tokens = [{ type: TokenTypes.String, value: "hello" }];
 
-  expect(parser(tokens)).toEqual(ast);
+    const ast = {
+      type: NodeTypes.Program,
+      body: [
+        {
+          type: NodeTypes.StringLiteral,
+          value: "hello",
+        },
+      ],
+    };
+    expect(parser(tokens)).toEqual(ast);
+  });
+
+  it("call expression (add 1 1)", () => {
+    const tokens = [
+      { type: TokenTypes.Paren, value: "(" },
+      { type: TokenTypes.Name, value: "add" },
+      { type: TokenTypes.Number, value: "1" },
+      { type: TokenTypes.Number, value: "1" },
+      { type: TokenTypes.Paren, value: ")" },
+    ];
+    const ast = {
+      type: NodeTypes.Program,
+      body: [
+        {
+          type: NodeTypes.CallExpression,
+          name: "add",
+          params: [
+            {
+              type: NodeTypes.NumberLiteral,
+              value: "1",
+            },
+            {
+              type: NodeTypes.NumberLiteral,
+              value: "1",
+            },
+          ],
+        },
+      ],
+    };
+
+    expect(parser(tokens)).toEqual(ast);
+  });
 });
