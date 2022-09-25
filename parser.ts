fb9e625263ab8a525c43b810cab51daa10841a08
Polaris <54269504@qq.com>
2022-09-25 13:55:56 +0800

    refac: parser

--- parser.ts
+++ parser.ts
@@ -1,84 +1,60 @@
+import {
+  createRootNode,
+  createStringLiteralNode,
+  createNumberLiteralNode,
+  createCallExpression,
+} from "./ast";
 import { Token, TokenTypes } from "./tokenizer";
 
-export enum NodeTypes {
-  Root,
-  Number,
-  CallExpression,
-}
-
-interface Node {
-  type: NodeTypes;
-}
-
-type ChildNode = NumberNode | CallExpression;
-
-interface NumberNode extends Node {
-  value: string;
-}
-
-interface RootNode extends Node {
-  body: ChildNode[];
-}
-
-interface CallExpression extends Node {
-  name: string;
-  params: ChildNode[];
-}
-
-function createRootNode(): RootNode {
-  return {
-    type: NodeTypes.Root,
-    body: [],
-  };
-}
-
-function createNumberNode(value: string): NumberNode {
-  return {
-    type: NodeTypes.Number,
-    value,
-  };
-}
-
-function createCallExpressionNode(name: string): CallExpression {
-  return {
-    type: NodeTypes.CallExpression,
-    name,
-    params: [],
-  };
-}
-
 export function parser(tokens: Token[]) {
+  const root = createRootNode();
+
   let current = 0;
-  const rootNode = createRootNode();
 
   function walk() {
     let token = tokens[current];
+
     if (token.type === TokenTypes.Number) {
-      // rootNode.body.push(numberNode);
       current++;
-      return createNumberNode(token.value);
+
+      return createNumberLiteralNode(token.value);
+    }
+
+    if (token.type === TokenTypes.String) {
+      current++;
+
+      return createStringLiteralNode(token.value);
     }
 
     if (token.type === TokenTypes.Paren && token.value === "(") {
       token = tokens[++current];
-      const node = createCallExpressionNode(token.value);
 
+      let node = createCallExpression(token.value);
+
+      // 上一个 token 已经使用完了  所以我们还需要在移动下位置
       token = tokens[++current];
-      while (!(token.type === TokenTypes.Paren && token.value === ")")) {
+      // params
+      while (
+        // token.type !== TokenType.paren ||
+        // (token.type === TokenType.paren && token.value !== ")")
+        !(token.type === TokenTypes.Paren && token.value === ")")
+      ) {
         node.params.push(walk());
         token = tokens[current];
       }
 
+      // 跳过 )
       current++;
+
       return node;
     }
 
-    throw new Error(`token is undefined: ${token}`);
+    throw new Error(`识别不了的 token: ${token}`);
   }
 
   while (current < tokens.length) {
-    rootNode.body.push(walk());
+    root.body.push(walk());
   }
 
-  return rootNode;
+  return root;
 }
